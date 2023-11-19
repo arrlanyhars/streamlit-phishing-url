f06a8d1149ae076bf5b60d778a42e8000c9a15be
lani268 <71999653+arrlanyhars@users.noreply.github.com>
2023-11-19 20:48:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -68,6 +68,14 @@ urls = st.text_input("", "")
 
 #--------------------------------------------------------------------------------------------#
 # APP
+# Add custom CSS to hide the GitHub icon
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+
 def process_tld(url):
     try:
 #         Extract the top level domain (TLD)
