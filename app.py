3dc99efea6ec99d5c1d6e62716452af20d9368e6
lani268 <71999653+arrlanyhars@users.noreply.github.com>
2023-11-19 21:28:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -70,11 +70,10 @@ urls = st.text_input("", "")
 # APP
 # Add custom CSS to hide the GitHub icon
 """
-hide_github_icon = """
-GithubIcon {
-  visibility: hidden;
+hide_github_icon =
+    GithubIcon {
+      visibility: hidden;
 }
-"""
 """
 st.markdown(hide_github_icon, unsafe_allow_html=True)
 
